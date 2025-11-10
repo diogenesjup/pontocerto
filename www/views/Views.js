122098c70e4912c49aaafc2b5dded3b6a760d663
Diogenes Junior <diogenesjunior.ti@gmail.com>
2025-11-10 20:27:40 -0300

    1011 2

--- www/views/Views.js
+++ www/views/Views.js
@@ -951,12 +951,28 @@ class Views{
             document.getElementById('btnViewCadastro').addEventListener('click', function() {
                const acaoCamera = localStorage.getItem("acaoAcionamentoCamera");
                
+              if (acaoCamera === 'rg_frontal' && imagensSelecionadas.length > 0) {
+                  enviarDocumentoParaWordPress('foto_documento_identificacao_frente');
+               } 
+
                if (acaoCamera === 'rg_tras' && imagensSelecionadas.length > 0) {
-                  enviarDocumentoParaWordPress('foto_documento_identificacao_tras');
-               } else {
-                  // Seu código original do botão aqui
-                  console.log('Nenhuma imagem para enviar ou ação diferente de rg_tras');
-               }
+                  enviarDocumentoParaWordPress('foto_documento_identificacao_atras');
+               } 
+
+               if (acaoCamera === 'selfie' && imagensSelecionadas.length > 0) {
+                  enviarDocumentoParaWordPress('selfie');
+               } 
+
+               /*
+               if (acaoCamera === 'selfie_segurando_documento' && imagensSelecionadas.length > 0) {
+                  enviarDocumentoParaWordPress('selfie_segurando_documento');
+               } 
+               */
+
+               if (acaoCamera === 'comprovante_endereco' && imagensSelecionadas.length > 0) {
+                  enviarDocumentoParaWordPress('comprovante_de_endereco');
+               } 
+               
             });
 
     }
@@ -1012,12 +1028,28 @@ class Views{
             document.getElementById('btnViewCadastro').addEventListener('click', function() {
                const acaoCamera = localStorage.getItem("acaoAcionamentoCamera");
                
+               if (acaoCamera === 'rg_frontal' && imagensSelecionadas.length > 0) {
+                  enviarDocumentoParaWordPress('foto_documento_identificacao_frente');
+               } 
+
+               if (acaoCamera === 'rg_tras' && imagensSelecionadas.length > 0) {
+                  enviarDocumentoParaWordPress('foto_documento_identificacao_atras');
+               } 
+
                if (acaoCamera === 'selfie' && imagensSelecionadas.length > 0) {
-                  enviarDocumentoParaWordPress('foto_documento_identificacao_selfie');
-               } else {
-                  // Seu código original do botão aqui
-                  console.log('Nenhuma imagem para enviar ou ação diferente de selfie');
-               }
+                  enviarDocumentoParaWordPress('selfie');
+               } 
+
+               /*
+               if (acaoCamera === 'selfie_segurando_documento' && imagensSelecionadas.length > 0) {
+                  enviarDocumentoParaWordPress('selfie_segurando_documento');
+               } 
+               */
+
+               if (acaoCamera === 'comprovante_endereco' && imagensSelecionadas.length > 0) {
+                  enviarDocumentoParaWordPress('comprovante_de_endereco');
+               } 
+               
             });
 
     }
@@ -1074,12 +1106,30 @@ class Views{
             document.getElementById('btnViewCadastro').addEventListener('click', function() {
                const acaoCamera = localStorage.getItem("acaoAcionamentoCamera");
                
+               if (acaoCamera === 'rg_frontal' && imagensSelecionadas.length > 0) {
+                  enviarDocumentoParaWordPress('foto_documento_identificacao_frente');
+               } 
+
+               if (acaoCamera === 'rg_tras' && imagensSelecionadas.length > 0) {
+                  enviarDocumentoParaWordPress('foto_documento_identificacao_atras');
+               } 
+
+               if (acaoCamera === 'selfie' && imagensSelecionadas.length > 0) {
+                  enviarDocumentoParaWordPress('selfie');
+               } 
+
+               /*
+               if (acaoCamera === 'selfie_segurando_documento' && imagensSelecionadas.length > 0) {
+                  enviarDocumentoParaWordPress('selfie_segurando_documento');
+               } 
+               */
+
                if (acaoCamera === 'comprovante_endereco' && imagensSelecionadas.length > 0) {
-                  enviarDocumentoParaWordPress('foto_documento_identificacao_selfie');
-               } else {
-                  // Seu código original do botão aqui
-                  console.log('Nenhuma imagem para enviar ou ação diferente de selfie');
-               }
+                  enviarDocumentoParaWordPress('comprovante_de_endereco');
+               } 
+               
+
+               
             });
 
     }
